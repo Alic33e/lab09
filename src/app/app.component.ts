6f500b8b364214ad78480c6b019db1c183af46d4
Alice <jeanpollsegura0402@gmail.com>
2024-05-09 12:09:14 -0500

    imagen-css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router'; // Ensure this import is correct
-import { SaludoComponent } from './saludo/saludo.component';
+import { RouterOutlet } from '@angular/router';
 import { ContadorComponent } from './contador/contador.component';
 import { GaleriaImagenesComponent } from './galeria-imagenes/galeria-imagenes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SaludoComponent, ContadorComponent, GaleriaImagenesComponent], // Ensure RouterOutlet is correctly referenced here
+  imports: [RouterOutlet, ContadorComponent, GaleriaImagenesComponent], // Ensure RouterOutlet is correctly referenced here
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
